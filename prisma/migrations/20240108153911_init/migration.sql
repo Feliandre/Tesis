257a182b43d51e052e6a172e0e8b35644abdba9a
Feliandres <85601974+Feliandres@users.noreply.github.com>
2024-01-08 10:54:29 -0500

    cambio base de datos
    cambio base de datos

---
+++ prisma/migrations/20240108153911_init/migration.sql
@@ -0,0 +1,111 @@
+-- CreateTable
+CREATE TABLE `Role` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `nombre` VARCHAR(50) NOT NULL,
+    `descripcion` VARCHAR(255) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `Role_nombre_key`(`nombre`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Gym` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `ruc` VARCHAR(13) NOT NULL,
+    `nombre` VARCHAR(50) NOT NULL,
+    `direccion` VARCHAR(150) NOT NULL,
+    `telefono` VARCHAR(20) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Plan` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `nombre` VARCHAR(50) NOT NULL,
+    `descripcion` VARCHAR(255) NOT NULL,
+    `valor` DECIMAL(5, 2) NOT NULL,
+    `duracion` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `Plan_nombre_key`(`nombre`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Member` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `cedula` VARCHAR(13) NOT NULL,
+    `nombre` VARCHAR(50) NOT NULL,
+    `apellido` VARCHAR(50) NOT NULL,
+    `email` VARCHAR(100) NOT NULL,
+    `telefono` VARCHAR(20) NOT NULL,
+    `telefonoEmergencia` VARCHAR(20) NOT NULL,
+    `fechaNacimiento` DATE NOT NULL,
+    `sexo` CHAR(2) NOT NULL,
+    `direccion` VARCHAR(255) NOT NULL,
+    `nacionalidad` VARCHAR(50) NOT NULL,
+    `fechaInscripcion` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `estado` INTEGER NULL DEFAULT 1,
+    `planId` INTEGER NOT NULL DEFAULT 1,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `Member_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Asistencia` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `fecha` DATETIME NOT NULL,
+    `miembroId` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Pago` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `fecha` DATE NOT NULL,
+    `miembroId` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Usuario` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `nombre` VARCHAR(50) NOT NULL,
+    `apellido` VARCHAR(50) NOT NULL,
+    `email` VARCHAR(100) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `estado` INTEGER NULL DEFAULT 1,
+    `rolId` INTEGER NULL DEFAULT 2,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `Usuario_email_key`(`email`),
+    UNIQUE INDEX `Usuario_rolId_key`(`rolId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Member` ADD CONSTRAINT `Member_planId_fkey` FOREIGN KEY (`planId`) REFERENCES `Plan`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Asistencia` ADD CONSTRAINT `Asistencia_miembroId_fkey` FOREIGN KEY (`miembroId`) REFERENCES `Member`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Pago` ADD CONSTRAINT `Pago_miembroId_fkey` FOREIGN KEY (`miembroId`) REFERENCES `Member`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Usuario` ADD CONSTRAINT `Usuario_rolId_fkey` FOREIGN KEY (`rolId`) REFERENCES `Role`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
